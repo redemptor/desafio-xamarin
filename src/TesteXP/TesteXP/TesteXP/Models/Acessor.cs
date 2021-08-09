b28c6da28be013c5b269625d7d1e39334f283dc7
Raphael Rodrigues <raphael.silva@live.com>
2021-08-09 13:06:08 -0300

    Ajustes models

--- src/TesteXP/TesteXP/TesteXP/Models/Acessor.cs
+++ src/TesteXP/TesteXP/TesteXP/Models/Acessor.cs
@@ -2,6 +2,12 @@
 {
     public class Acessor
     {
+        public Acessor(uint codigo, string nome)
+        {
+            Codigo = codigo;
+            Nome = nome;
+        }
+
         public uint Codigo { get; set; }
         public string Nome { get; set; }
     }
--- src/TesteXP/TesteXP/TesteXP/Models/Ordem.cs
+++ src/TesteXP/TesteXP/TesteXP/Models/Ordem.cs
@@ -5,20 +5,59 @@ namespace TesteXP.Models
 {
     public class Ordem
     {
+        public Ordem() { }
+
+        public Ordem(
+            ulong id,
+            DateTime dataHora,
+            Acessor assessor,
+            uint conta,
+            string ativo,
+            EnumTipo tipo,
+            int quantidade,
+            int quantidadeAparente,
+            int quantidadeDisponivel,
+            int quantidadeCancelada,
+            int quantidadeExecutada,
+            decimal valor,
+            decimal valorDisparo,
+            decimal objetivo,
+            decimal objetivoDisparo,
+            double reducao)
+        {
+            Id = id;
+            DataHora = dataHora;
+            Assessor = assessor;
+            Conta = conta;
+            Ativo = ativo;
+            Tipo = tipo;
+            Quantidade = quantidade;
+            QuantidadeAparente = quantidadeAparente;
+            QuantidadeDisponivel = quantidadeDisponivel;
+            QuantidadeCancelada = quantidadeCancelada;
+            QuantidadeExecutada = quantidadeExecutada;
+            Valor = valor;
+            ValorDisparo = valorDisparo;
+            Objetivo = objetivo;
+            ObjetivoDisparo = objetivoDisparo;
+            Reducao = reducao;
+        }
+
+        public ulong Id { get; set; }
         public DateTime DataHora { get; set; }
         public Acessor Assessor { get; set; }
         public uint Conta { get; set; }
         public string Ativo { get; set; }
         public EnumTipo Tipo { get; set; }
-        public uint Quantidade { get; set; }
-        public uint QuantidadeAparente { get; set; }
-        public uint QuantidadeDisponivel { get; set; }
-        public uint QuantidadeCancelada { get; set; }
-        public uint QuantidadeExecutada { get; set; }
+        public int Quantidade { get; set; }
+        public int QuantidadeAparente { get; set; }
+        public int QuantidadeDisponivel { get; set; }
+        public int QuantidadeCancelada { get; set; }
+        public int QuantidadeExecutada { get; set; }
         public decimal Valor { get; set; }
         public decimal ValorDisparo { get; set; }
         public decimal Objetivo { get; set; }
         public decimal ObjetivoDisparo { get; set; }
-        public float Reducao { get; set; }
+        public double Reducao { get; set; }
     }
 }
