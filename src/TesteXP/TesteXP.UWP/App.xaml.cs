655455eb72c53d1f8ab983f0a286aa36e4346f74
Raphael Rodrigues <raphael.silva@live.com>
2021-08-11 07:59:44 -0300

    Ajuste estrutura de diretorios

--- src/TesteXP/TesteXP.UWP/App.xaml.cs
+++ src/TesteXP/TesteXP.UWP/App.xaml.cs
@@ -42,7 +42,7 @@ namespace TesteXP.UWP
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
 
