5d584b91b8fcd51cba552ab073a26f0a5e81082f
Raphael Rodrigues <raphael.silva@live.com>
2021-08-13 13:12:09 -0300

    Add converter de celula vazia

---
+++ src/TesteXP/TesteXP/Converters/CellEmptyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TesteXP.Converters
+{
+    public class CellEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrWhiteSpace(value?.ToString())
+                ? "-"
+                : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() == "-"
+                ? 0
+                : value;
+        }
+    }
+}
