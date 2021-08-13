f973197564abee6ca5f3864ecc3ea0cd12df64e9
Raphael Rodrigues <raphael.silva@live.com>
2021-08-13 14:16:37 -0300

    add testes

---
+++ tests/TesteXP.Test/Models/AssessorTests.cs
@@ -0,0 +1,20 @@
+﻿using TesteXP.Models;
+using Xunit;
+
+namespace TesteXP.Test.Models
+{
+    public class AssessorTests
+    {
+        [Fact]
+        public void DeveTerToStringValido()
+        {
+            var codigo = 10u;
+            var nome = "João Silva";
+            var toStringEsperado = $"[{codigo:000}] {nome}";
+
+            var assessor = new Assessor(codigo, nome);
+
+            Assert.Equal(toStringEsperado, assessor.ToString());
+        }
+    }
+}
--- tests/TesteXP.Test/ServicoDeHistoricoTests.cs
+++ tests/TesteXP.Test/ServicoDeHistoricoTests.cs
@@ -1,14 +1,34 @@
 using System;
+using System.Linq;
+using TesteXP.Services;
 using Xunit;
 
 namespace TesteXP.Test
 {
     public class ServicoDeHistoricoTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private ServicoDeHistorico _servicoDeHistorico;
 
+        public ServicoDeHistoricoTests()
+        {
+            _servicoDeHistorico = new ServicoDeHistorico();
+        }
+
+        [Fact]
+        public void DeveRetornarVariasOrdensConsultaInicial()
+        {
+            var retorno = _servicoDeHistorico.ObterOrdens();
+
+            Assert.True(retorno.Count() > 1, "Esperado mais de um item.");
+        }
+
+        [Fact]
+        public void DeveRetornarApenasUmaOrdem()
+        {
+            _ = _servicoDeHistorico.ObterOrdens();
+            var retorno = _servicoDeHistorico.ObterOrdens();
+
+            Assert.True(retorno.Count() == 1, "Esperado apenas um item.");
         }
     }
 }
